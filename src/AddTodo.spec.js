8bd93157f60195c03c9a70149b2224f319bf6f06
Vishesh Singh <VisheshSingh16@gmail.com>
2020-04-05 08:42:26 -0500

    Use the  🧐 debug method

--- src/AddTodo.spec.js
+++ src/AddTodo.spec.js
@@ -1,19 +1,9 @@
-// import React from 'react'
-// import { render } from '@testing-library/react'
-// import user from '@testing-library/user-event'
-// import AddTodo from './AddTodo'
+import React from 'react'
+import { render } from '@testing-library/react'
+import AddTodo from './AddTodo'
 
-// it('should accepts values for the new todo ', () => {
-//   const mockAddTodo = jest.fn()
-//   const { getByLabelText, getByText } = render(
-//     <AddTodo addTodo={mockAddTodo} />
-//   )
-//   const Input = getByLabelText(/add todo/i)
-//   user.type(Input, 'a new todo')
-//   expect(Input).toHaveValue('a new todo')
-
-//   const SubmitBtn = getByText(/add todo/i)
-//   user.click(SubmitBtn)
-//   expect(mockAddTodo).toHaveBeenCalled()
-//   expect(mockAddTodo).toHaveBeenCalledTimes(1)
-// })
+it('shoud accept values to add new todo', () => {
+  const mockAddTodo = jest.fn()
+  const { debug } = render(<AddTodo addTodo={mockAddTodo} />)
+  debug()
+})
