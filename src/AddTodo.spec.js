8bcbde146148e2448190d799e79be443c778f49c
Vishesh Singh <VisheshSingh16@gmail.com>
2020-04-05 09:01:11 -0500

    import fireEvent from rect-testing-library 📚

--- src/AddTodo.spec.js
+++ src/AddTodo.spec.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import AddTodo from './AddTodo'
 
 it('shoud accept values to add new todo', () => {
