f4805da8adab0511ba3bf6b540c203aa3457f4f3
Vishesh Singh <VisheshSingh16@gmail.com>
2020-04-05 08:59:07 -0500

    fireEvent  🔥

--- src/AddTodo.spec.js
+++ src/AddTodo.spec.js
@@ -9,5 +9,13 @@ it('shoud accept values to add new todo', () => {
   )
   // Get the Input field
   const Input = getByLabelText(/add todo/i)
+  // Fire a change event on input field
+  fireEvent.change(Input, { target: { value: 'a new todo' } })
+  // Assertion to check if input value matches
+  expect(Input.value).toContain('a new todo')
+  // Get the submit button
+  const SubmitBtn = getByText(/add todo/i)
+  // fire a click event
+  fireEvent.click(SubmitBtn)
   debug()
 })
